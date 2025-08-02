5aea30b83b11f5199fac8ac7dbefe16051c4a12d
CedBisaya <sedorikku87@gmail.com>
2025-08-02 22:01:24 +0800

    made member page  route

--- resources/views/swisa-admin/members.blade.php
+++ resources/views/swisa-admin/members.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div>
-    Registered Members</div>
+        Registered Members
+    </div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Members') }}
