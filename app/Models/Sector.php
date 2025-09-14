48f1ec937211bde8cdf05efbac98a44ea388ae3e
CedBisaya <sedorikku87@gmail.com>
2025-09-15 00:27:23 +0800

    I Forgot to make the sector model hehe, i added it na

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use App\Models\UserInfo;
+use App\Models\Training;
 
 class Sector extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'sector_name',
+    ];
+
+    //RELATIONSHIPS
+
+    //a sector can have many users
+    public function userInfo(){
+        return $this->hasMany(UserInfo::class);
+    }
+
+    //sector can have many trainings
+    public function trainings(){
+        return $this->hasMany(Training::class);
+    }
 }
